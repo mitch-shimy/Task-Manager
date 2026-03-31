69c67e84903d70d81269f12db4a307d00e54f4a9
mitch-shimy <mitch.shimy@gmail.com>
2026-03-31 21:08:23 +0300

    Update init-db.js

--- backend/init-db.js
+++ backend/init-db.js
@@ -1,9 +1,22 @@
 const fs = require("fs");
-const { execSync } = require("child_process");
+const sqlite3 = require("sqlite3").verbose();
 
 if (!fs.existsSync("tasks.db")) {
   console.log("Initializing database...");
-  execSync('sqlite3 tasks.db ".read dump.sql"', { stdio: "inherit" });
+
+  const db = new sqlite3.Database("tasks.db");
+
+  const sql = fs.readFileSync("dump.sql", "utf-8");
+
+  db.exec(sql, (err) => {
+    if (err) {
+      console.error("DB init error:", err);
+    } else {
+      console.log("Database initialized successfully");
+    }
+    db.close();
+  });
+
 } else {
   console.log("Database already exists. Skipping init.");
 }
